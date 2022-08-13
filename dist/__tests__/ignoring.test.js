60ba24d9e3873a41ff047932c020390d2f2511dc
Melissa Rose <123melissa321@gmail.com>
2022-08-13 10:16:41 +0100

    Added build script

---
+++ dist/__tests__/ignoring.test.js
@@ -0,0 +1,121 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const supertest_1 = __importDefault(require("supertest"));
+const model_1 = require("../digipet/model");
+const server_1 = __importDefault(require("../server"));
+/**
+ * This file has integration tests for ignoring a digipet.
+ *
+ * It is intended to test three behaviours:
+ *  1. ignoring a digitpet leads to decreasing happiness
+ *  2. ignoring a digipet leads to decreasing nutrition
+ *  3. ignoring a digipet leads to decreasing discipline
+ */
+describe("When a user ignores a digipet repeatedly, its happiness decreases by 10 each time until it eventually floors at 0", () => {
+    beforeAll(() => {
+        // setup: give an initial digipet
+        const startingDigipet = {
+            happiness: 25,
+            nutrition: 75,
+            discipline: 60,
+        };
+        model_1.setDigipet(startingDigipet);
+    });
+    test("GET /digipet informs them that they have a digipet with expected stats", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet");
+        expect(response.body.message).toMatch(/your digipet/i);
+        expect(response.body.digipet).toHaveProperty("happiness", 25);
+    }));
+    test("1st GET /digipet/ignore informs them about the ignoring and shows decreased happiness for digipet", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("happiness", 15);
+    }));
+    test("2nd GET /digipet/ignore shows continued stats change", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("happiness", 5);
+    }));
+    test("3rd GET /digipet/ignore shows happiness hitting a floor of 0", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("happiness", 0);
+    }));
+    test("4th GET /digipet/ignore shows no further decrease in happiness", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("happiness", 0);
+    }));
+});
+describe("When a user ignores a digipet repeatedly, its nutrition decreases by 10 each time until it eventually floors at 0", () => {
+    beforeAll(() => {
+        // setup: give an initial digipet
+        const startingDigipet = {
+            happiness: 55,
+            nutrition: 25,
+            discipline: 60,
+        };
+        model_1.setDigipet(startingDigipet);
+    });
+    test("GET /digipet informs them that they have a digipet with expected stats", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet");
+        expect(response.body.message).toMatch(/your digipet/i);
+        expect(response.body.digipet).toHaveProperty("nutrition", 25);
+    }));
+    test("1st GET /digipet/ignore informs them about the ignoring and shows decreased nutrition for digipet", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("nutrition", 15);
+    }));
+    test("2nd GET /digipet/ignore shows continued stats change", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("nutrition", 5);
+    }));
+    test("3rd GET /digipet/ignore shows nutrition hitting a floor of 0", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("nutrition", 0);
+    }));
+    test("4th GET /digipet/ignore shows no further decrease in nutrition", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("nutrition", 0);
+    }));
+});
+describe("When a user ignores a digipet repeatedly, its discipline decreases by 10 each time until it eventually floors at 0", () => {
+    beforeAll(() => {
+        // setup: give an initial digipet
+        const startingDigipet = {
+            happiness: 55,
+            nutrition: 75,
+            discipline: 25,
+        };
+        model_1.setDigipet(startingDigipet);
+    });
+    test("GET /digipet informs them that they have a digipet with expected stats", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet");
+        expect(response.body.message).toMatch(/your digipet/i);
+        expect(response.body.digipet).toHaveProperty("discipline", 25);
+    }));
+    test("1st GET /digipet/ignore informs them about the ignoring and shows decreased discipline for digipet", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("discipline", 15);
+    }));
+    test("2nd GET /digipet/ignore shows continued stats change", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("discipline", 5);
+    }));
+    test("3rd GET /digipet/ignore shows discipline hitting a floor of 0", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("discipline", 0);
+    }));
+    test("4th GET /digipet/ignore shows no further decrease in discipline", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield supertest_1.default(server_1.default).get("/digipet/ignore");
+        expect(response.body.digipet).toHaveProperty("discipline", 0);
+    }));
+});
